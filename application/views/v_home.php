e6cbc43e5e6fecb13523874b17abcd55f6b0707a
happypeoplee <raidavr7@gmail.com>
2022-09-07 20:54:15 +0700

    menambahkan link permintaan perubahan data

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -394,7 +394,7 @@
                                                                                     ?>">disini</a></p>
           </div>
           <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-            <div class="icon"><i class="bx bx-align-justify"></i></div>
+            <div class="icon"><i class="bx bx-edit-alt"></i></div>
             <h4 class="title">Permintaan Perubahan Data</h4>
             <p class="description">Klik link <a href="<?php echo base_url() . 'Permintaan' 
                                                                                     ?>">disini</a></p>
