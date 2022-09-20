bb8d9bf1e7adc39f409cb696ca6d67d6d43b9c86
happypeoplee <raidavr7@gmail.com>
2022-09-20 20:57:09 +0700

    function baru

--- application/models/M_Keuangan.php
+++ application/models/M_Keuangan.php
@@ -9,7 +9,17 @@ class M_Keuangan extends CI_Model
 
     public function total_keuangan()
     {
-        return $this->db->query("SELECT sum(nominal) as total FROM pemasukan_keuangan");
+        return $this->db->query("SELECT sum(uang_masuk) as total FROM pemasukan_keuangan");
+    }
+
+    public function saldo_akhir()
+    {
+        return $this->db->query("SELECT saldo_akhir as saldo FROM laporan_keuangan ORDER BY id_laporan_keuangan DESC");
+    }
+
+    public function menampilkan_laporan()
+    {
+        return $this->db->query("SELECT * FROM laporan_keuangan INNER JOIN pemasukan_keuangan ON laporan_keuangan.id_pemasukan = pemasukan_keuangan.id_pemasukan ORDER BY id_laporan_keuangan DESC");
     }
 
     public function insert_record($data, $table)
