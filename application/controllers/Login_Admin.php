2e9d6f3c87fc78727b81a58058983756e2b537ec
happypeoplee <raidavr7@gmail.com>
2022-10-05 21:25:25 +0700

    update

--- application/controllers/Login_Admin.php
+++ application/controllers/Login_Admin.php
@@ -6,7 +6,7 @@ class Login_Admin extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model(array('M_Admin'));
+        $this->load->model(array('M_User'));
     }
 
     function index()
@@ -23,19 +23,20 @@ class Login_Admin extends CI_Controller
             'username' => $username
         );
 
-        //   $cek_status = $this->M_Admin->cek_status($username)->num_rows();
-        $cek_login = $this->M_Admin->cek_login('user', $where)->row_array();
+        //   $cek_status = $this->M_User->cek_status($username)->num_rows();
+        $cek_login = $this->M_User->cek_login('user', $where)->row_array();
 
         if ($cek_login) {
             if ($cek_login['status_user'] == 1) {
                 if (password_verify($password, $cek_login['password'])) {
                     $session = array(
                         'username' => $cek_login['username'],
+                        'level_user' => $cek_login['id_level_user'],
                         'status' => 'login'
                     );
 
                     $this->session->set_userdata($session);
-                    redirect('Dashboard');
+                    redirect('admin/dashboard');
                 } else {
                     $this->session->set_flashdata('info', 'Password salah');
                     redirect('login');
