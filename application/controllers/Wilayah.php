171b8e6d706c3edcf4b44873a6642bfe4fb054c6
happypeoplee <raidavr7@gmail.com>
2022-10-07 20:19:45 +0700

    update

--- application/controllers/Wilayah.php
+++ application/controllers/Wilayah.php
@@ -17,9 +17,8 @@ class Wilayah extends CI_Controller
     public function index()
     {
         $data['title'] = "Wilayah";
-        $data['wilayah'] = $this->M_Wilayah->tampil()->result();
-        $data['jemaat'] = $this->M_Anggota_Jemaat->tampil()->result();
         $this->load->view('templates/header.php', $data);
+        $this->load->view('templates/sidebar.php');
         $this->load->view('admin/wilayah/v_lihat_wilayah.php', $data);
     }
 
@@ -27,6 +26,24 @@ class Wilayah extends CI_Controller
     {
         $wilayah = $this->M_Wilayah->tampil()->result();
         echo json_encode($wilayah);
+    }
+
+    public function nama_jemaat()
+    {
+        if (!isset($_POST['searchJemaat'])) {
+            $nama = $this->db->query("SELECT id_anggota, nama_lengkap_anggota, nama_wilayah FROM anggota_jemaat INNER JOIN wilayah ON anggota_jemaat.id_wilayah = Wilayah.id_wilayah")->result_array();
+        } else {
+            $search = strtolower($_POST['searchJemaat']);
+            $nama = $this->db->query("SELECT id_anggota, nama_lengkap_anggota, nama_wilayah FROM anggota_jemaat INNER JOIN wilayah ON anggota_jemaat.id_wilayah = Wilayah.id_wilayah WHERE nama_lengkap_anggota LIKE '%$search%'")->result_array();
+        }
+
+        $list = array();
+        for ($i = 0; $i < count($nama); $i++) {
+            $list[$i]['id'] = $nama[$i]['id_anggota'];
+            $list[$i]['text'] = $nama[$i]['nama_lengkap_anggota'] . ' - ' . $nama[$i]['nama_wilayah'];
+        }
+
+        echo json_encode($list);
     }
 
     public function tambah_wilayah()
@@ -52,8 +69,28 @@ class Wilayah extends CI_Controller
         $data['title'] = "Wilayah";
         $data['wilayahEdit'] = $this->M_Wilayah->tampil_edit($id_wilayah)->result();
         $data['jemaat'] = $this->M_Anggota_Jemaat->tampil()->result();
+        $data['id_wilayah'] = $id_wilayah;
         $this->load->view('templates/header.php', $data);
+        $this->load->view('templates/sidebar.php');
         $this->load->view('admin/wilayah/v_edit_wilayah', $data);
+    }
+
+    public function nama_jemaat_per_wilayah($id_wilayah)
+    {
+        if (!isset($_POST['searchJemaat'])) {
+            $nama = $this->db->query("SELECT id_anggota, nama_lengkap_anggota, nama_wilayah FROM anggota_jemaat INNER JOIN wilayah ON anggota_jemaat.id_wilayah = Wilayah.id_wilayah WHERE anggota_jemaat.id_wilayah = '$id_wilayah'")->result_array();
+        } else {
+            $search = strtolower($_POST['searchJemaat']);
+            $nama = $this->db->query("SELECT id_anggota, nama_lengkap_anggota, nama_wilayah FROM anggota_jemaat INNER JOIN wilayah ON anggota_jemaat.id_wilayah = Wilayah.id_wilayah WHERE nama_lengkap_anggota LIKE '%$search%' AND anggota_jemaat.id_wilayah = '$id_wilayah'")->result_array();
+        }
+
+        $list = array();
+        for ($i = 0; $i < count($nama); $i++) {
+            $list[$i]['id'] = $nama[$i]['id_anggota'];
+            $list[$i]['text'] = $nama[$i]['nama_lengkap_anggota'] . ' - ' . $nama[$i]['nama_wilayah'];
+        }
+
+        echo json_encode($list);
     }
 
     public function proses_edit_wilayah()
